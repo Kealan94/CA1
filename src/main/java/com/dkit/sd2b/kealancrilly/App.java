8d293d5ced34ba6476f4d6bddd78d65879ce603a
Kealan Crilly <crilly2k12@hotmail.co.uk>
2020-10-27 20:56:32 +0000

    Started creating for loops to select five grades.

--- src/main/java/com/dkit/sd2b/kealancrilly/App.java
+++ src/main/java/com/dkit/sd2b/kealancrilly/App.java
@@ -1,3 +1,4 @@
+//D00224427 Kealan Crilly
 package com.dkit.sd2b.kealancrilly;
 import java.io.IOException;
 import java.util.Scanner;
@@ -7,14 +8,12 @@ import java.io.FileNotFoundException;
 public class App {
 
     public static void main(String[] args) throws IOException {
-        //int selectFiveGrades();
-
-        double calculateAverage;
-        //readStudentFile("JC_Results.txt");
-        readLineByLine ("JC_Results.txt");
+        //selectFiveGrades();
+        //calculateAverage();
+        readLineByLine("JC_Results.txt");
     }
-    public static void readLineByLine(String file)
-    {
+
+    public static void readLineByLine(String file) {
         System.out.println("Read and output text file line-by-line");
 
         try {
@@ -28,60 +27,49 @@ public class App {
         } catch (IOException e) {
         }
     }
-    /*public static void readStudentFile(String File) {
-
-        System.out.println("Reading in the student numbers and marks");
-
-        try {
-            Scanner sc = new Scanner(new File("C:\\Users\\Kealan\\IdeaProjects\\CA1\\src\\main\\java\\JC_Results.txt"));
-            //sc.useDelimiter("[,\r\n]+");
-            while (sc.hasNext()) {
-
-                String Student_Number = sc.nextLine();
-                String Subject_Code = sc.nextLine();
-                Integer Mark = sc.nextInt();
-                System.out.println(Student_Number);
-                System.out.println(Subject_Code);
-                System.out.println(Mark);
-            }
-            sc.close();
-
-        } catch (FileNotFoundException e) {
-            e.printStackTrace();
-            System.out.println("An error occurred, the file could not be found.");
-        }
-    }*/
-
-    private double calculateAverage(int[] selectedGrades) {
-        {
-
-        }
-        return 3.5;
-    }
-
-
 
     public int selectFiveGrades(int codes[], int grades[]) {
         codes = new int[1];
-        codes[0] = 6788;
-        codes[1] = 6799;
+        codes[0] = 891234;
+        codes[1] = 783461;
 
         grades = new int[8];
-        grades[0] = 54;
-        grades[1] = 43;
-        grades[2] = 83;
-        grades[3] = 27;
-        grades[4] = 39;
-        grades[5] = 50;
-        grades[6] = 61;
+        grades[0] = 65;
+        grades[1] = 58;
+        grades[2] = 45;
+        grades[3] = 60;
+        grades[4] = 50;
+        grades[5] = 48;
+        grades[6] = 42;
+        grades[7] = 42;
         {
             for (int i = 0; i < codes.length; i++) {
                 {
                     System.out.println("codes" + "");
                 }
             }
-            return 4657;
+            for (int i = 0; i < grades.length - 3; i++) {
+                while (grades[i] == codes[1]) {
+                    System.out.println(codes);
+                }
+                {
+                    if (grades[i] != codes[1]) {
+                        {
+                            System.out.println("grades" + grades);
+                        }
+                    }
+                }
+            }
+
+            //private double calculateAverage(int[] selectedGrades){
+            int sum = 0;
+            int total = 0;
         }
 
+                 return 0;
     }
 }
+
+
+
+
