579a8f814466e16594a40aab6e9b82c994bbf52e
Kealan Crilly <crilly2k12@hotmail.co.uk>
2020-10-27 12:06:24 +0000

    changed input for reading text file. And working on select five grades method.

--- src/main/java/com/dkit/sd2b/kealancrilly/App.java
+++ src/main/java/com/dkit/sd2b/kealancrilly/App.java
@@ -1,46 +1,45 @@
 package com.dkit.sd2b.kealancrilly;
+import java.io.IOException;
 import java.util.Scanner;
-import java.io.File;  // Import the File class
-import java.io.FileNotFoundException;  // Import this class to handle errors
+import java.io.File;
+import java.io.FileNotFoundException;
 
 public class App {
 
-    public static void main(String[] args) {
-
+    public static void main(String[] args) throws IOException {
+        int selectFiveGrades;
+        double calculateAverage;
         readWordsAndNumbers("JC_Results.txt");
     }
+
     public static void readWordsAndNumbers(String File) {
 
-        System.out.println("Reading mixture of words, and numbers from text file");
+        System.out.println("Reading in the student codes and marks");
 
         try {
-
             Scanner sc = new Scanner(new File("C:\\Users\\Kealan\\IdeaProjects\\CA1\\src\\main\\java\\JC_Results.txt"));
 
             while (sc.hasNext()) {
-                String str = sc.nextLine();
-                System.out.println(str);
+                String Student_Number = sc.nextLine();
+                String Subject_Code = sc.nextLine();
+                System.out.println(Student_Number);
+                System.out.println(Subject_Code);
             }
             sc.close();
 
         } catch (FileNotFoundException e) {
             e.printStackTrace();
-            System.out.println("An error occurred.");
-            e.printStackTrace();
+            System.out.println("An error occurred, the file could not be found.");
         }
     }
-}
 
-/*public class App {
 
-    public static void main(String[] args) {
-        int selectFiveGrades;
-        double calculateAverage;
-    }
-
-    ;
-
-    public void selectFiveGrades(int[] codes, int[] grades) {
+    public int[] selectFiveGrades(int[] codes, int[] grades) {
+        for (int i = 0; i<codes.length; i++){
+            if(codes[i]==English)
+        }
+        {
+            System.out.println("codes" + codes);
         codes = new int[10];
         grades = new int[10];
 
@@ -50,4 +49,6 @@ public class App {
         selectedGrades = new int[5];
     }
 }
- */
+        return  selectedGrades;
+
+}
